f9e3a29b55cb06a2c0d7d4cb2c566af98ddc24a7
Kittithat Thongluea <kittithat.thon@bumail.net>
2026-09-01 06:49:38 +0000

    Implement traversal functions for Singly, Doubly, and Circular Linked Lists

--- Lab_Part1_Singly_Linked_List.py
+++ Lab_Part1_Singly_Linked_List.py
@@ -6,9 +6,11 @@ class Node:
         self.next = None
 
 def traverse_singly(head):
-    """
-    ฟังก์ชันสำหรับสำรวจข้อมูล (Traversal) ของ Singly Linked List
-    """
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end="->")
+        currentNode = currentNode.next
+    print("null")
     # HINT 1: การสำรวจข้อมูล ควรสร้างตัวแปรใหม่มารับค่า head (เช่น currentNode = head) 
     # เพื่อป้องกันไม่ให้โครงสร้างลิสต์เดิมพัง จากนั้นใช้ลูป while currentNode:[cite: 1]
     # HINT 2: ภายในลูป while บรรทัดสุดท้ายต้องอัปเดตตำแหน่งเสมอด้วย currentNode = currentNode.next 
@@ -18,6 +20,16 @@ def traverse_singly(head):
     pass
 
 print("--- ผลลัพธ์ส่วนที่ 1: Singly Linked List ---")
+node1 = Node(9)
+node2 = Node(1)
+node3 = Node(7)
+node4 = Node(8)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+traverse_singly(node1)
 # HINT 3: การเชื่อมโหนด (Linking) ควรสร้าง object ของ Node ให้ครบทุกตัวก่อน 
 # แล้วค่อยจับคู่โดยระบุว่า node.next ชี้ไปที่ใคร[cite: 1]
 
